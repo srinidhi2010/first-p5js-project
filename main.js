41dc8f6a39506f17aa070f806d0f6eaae442f9ae
srinidhi2010 <109321775+srinidhi2010@users.noreply.github.com>
2023-09-24 09:23:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,172 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(640, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+
+
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480);
+    
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 270, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 290, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 310, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 330, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 350, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 370, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 390, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 410, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 430, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 450, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 470, 30);
+
+
+    fill("#c8fedc");
+    stroke("#c8fedc");
+circle(625, 250, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 230, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 210, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 190, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 170, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 150, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 130, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 110, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 90, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 70, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 50, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(625, 30, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(625, 10, 30);
+
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 270, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 290, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 310, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 330, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 350, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 370, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 390, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 410, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 430, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 450, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 470, 30);
+
+
+    fill("#c8fedc");
+    stroke("#c8fedc");
+circle(15, 250, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 230, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 210, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 190, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 170, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 150, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 130, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 110, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 90, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 70, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 50, 30);
+fill("#AEE7F8");
+stroke("#AEE7F8");
+circle(15, 30, 30);
+fill("#c8fedc");
+stroke("#c8fedc");
+circle(15, 10, 30);
+
+}
+
+function take_snapshot(){
+    save('student_name.png');
+}
